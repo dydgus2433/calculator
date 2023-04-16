c28925a15cae7f89f8bf41bd6b288030b149ba72
yh <dydgus2433@naver.com>
2023-04-16 09:58:57 +0900

    프로젝트 구조 수정 및 bmi 계산기 수정

---
+++ bmi_calculator/main.js
@@ -0,0 +1,11 @@
+function onSubmit(event){
+    event.preventDefault();
+    const w = parseFloat(event.target[0].value);
+    const h = parseFloat(event.target[1].value);
+
+
+    if((isNaN(w) || isNaN(h)) || (w <= 0 || h <= 0)){
+        alert('적절한 값이 아닙니다.')
+        return;
+    }
+}
